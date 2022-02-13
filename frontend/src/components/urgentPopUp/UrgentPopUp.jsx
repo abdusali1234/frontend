e5a89dd5bc637f4fb94568a3b279286aaff19137
Catarina Alves <catarina._3@hotmail.com>
2022-02-13 09:40:19 +0000

    improve

--- frontend/src/components/urgentPopUp/UrgentPopUp.jsx
+++ frontend/src/components/urgentPopUp/UrgentPopUp.jsx
@@ -7,7 +7,7 @@ function UrgentPopUp(props) {
       <div className="urgent-pop-up" onClick={props.changeCloseStat}>
           <div className="urgent-pop-up-child">
             <div className="urgent-i"><a href="tel:05076790000"><Phone className="u-img" /></a></div>
-            <div className="sub-detail">Call&nbsp; <a href="tel:05076790000">020 7679 0000</a>&nbsp; or UCL ext 30000</div>
+            <div className="sub-detail"><span>Call&nbsp; <a href="tel:05076790000">020 7679 0000</a></span> or UCL ext 30000</div>
           </div>
       </div>
 
