7bb6d28db0850fdc85aeaccbc59a116ddafc1654
FrancescaParrotta <99541012+FrancescaParrotta@users.noreply.github.com>
2022-02-13 00:17:42 +0000

    update toilet page

--- frontend/src/routes/toilet/Toilet.jsx
+++ frontend/src/routes/toilet/Toilet.jsx
@@ -9,3 +9,34 @@ function Toilet() {
 }
 
 export default Toilet;
+
+import './Toilet.css';
+// import EventElement from '../../components/eventElement/EventElement';
+import {ReactComponent as Toilet} from "../../icon/Toilet.svg"
+import {ReactComponent as Toiletpaper} from "../../icon/Toiletpaper.svg"
+import {ReactComponent as Dryer} from "../../icon/Dryer.svg"
+import {ReactComponent as Trashcan} from "../../icon/Trashcan.svg"
+import {ReactComponent as Other} from "../../icon/Other.svg"
+
+import EventElementBox from '../../components/eventElementBox/EventElementBox';
+import EventHead from '../../components/eventHead/EventHead';
+
+function Test() {
+
+  const events = [
+    { id: 1, eventsName: "Toilet", icon: <Toilet className="img" /> },
+    { id: 2, eventsName: "Toiletpaper", icon: <Toiletpaper className="img" /> },
+    { id: 3, eventsName: "Dryer", icon: <Dryer className="img" /> },
+    { id: 4, eventsName: "Trashcan", icon: <Trashcan className="img" /> },
+    { id: 5, eventsName: "Other", icon: <Other className="img" /> },
+  ];  
+
+  return (
+    <div className="Toilet">
+      <EventHead eventname="id18"/>
+      <EventElementBox events={events}/>
+    </div>
+  );
+}
+
+export default Toilet;
