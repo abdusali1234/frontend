c0645919900fcdd677a1ccb04a1e595a4bb8491f
Catarina Alves <catarina._3@hotmail.com>
2022-02-13 07:49:43 +0000

    Toilet comments

--- frontend/src/routes/toilet/Toilet.jsx
+++ frontend/src/routes/toilet/Toilet.jsx
@@ -1,23 +1,27 @@
+// Import css information
 import "./Toilet.css";
-// import EventElement from '../../components/eventElement/EventElement';
+
+// Imported saved icons
 import {ReactComponent as Toiletbowl} from "../../icon/Toiletbowl.svg"
 import {ReactComponent as Toiletpaper} from "../../icon/Toiletpaper.svg"
 import {ReactComponent as Dryer} from "../../icon/Dryer.svg"
 import {ReactComponent as Trashcan} from "../../icon/Trashcan.svg"
 import {ReactComponent as Other} from "../../icon/Other.svg"
 
+// Import Elements needed for the page
 import EventElementBox from '../../components/eventElementBox/EventElementBox';
 import EventHead from '../../components/eventHead/EventHead';
 
+// Construct webpage
 function Toilet() {
-
+  // Add the events
   const events = [
     { id: 1, eventsName: "Toiletbowl", icon: <Toiletbowl className="img" /> },
     { id: 2, eventsName: "Toiletpaper", icon: <Toiletpaper className="img" /> },
     { id: 3, eventsName: "Dryer", icon: <Dryer className="img" /> },
     { id: 4, eventsName: "Trashcan", icon: <Trashcan className="img" /> },
     { id: 5, eventsName: "Other", icon: <Other className="img" /> },
-  ];  
+  ];
 
   return (
     <div className="Toilet">
