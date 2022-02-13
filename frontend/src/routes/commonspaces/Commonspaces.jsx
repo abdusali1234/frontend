ebdad468d96d46182b12231aca1dadad8cb25346
Catarina Alves <catarina._3@hotmail.com>
2022-02-13 05:31:12 +0000

    Maybe finish commonspaces

--- frontend/src/routes/commonspaces/Commonspaces.jsx
+++ frontend/src/routes/commonspaces/Commonspaces.jsx
@@ -3,7 +3,6 @@ import './Commonspaces.css';
 import {ReactComponent as Microwave} from "../../icon/Microwave.svg"
 import {ReactComponent as Dirty} from "../../icon/Dirty-floors.svg"
 import {ReactComponent as Trash} from "../../icon/Trashcan.svg"
-import {ReactComponent as Door} from "../../icon/Door.svg"
 import {ReactComponent as GeneralDirt} from "../../icon/Generaldirt.svg"
 import {ReactComponent as VendingMachine} from "../../icon/Vendingmachines.svg"
 import {ReactComponent as WaterFountain} from "../../icon/Waterfountain.svg"
@@ -18,11 +17,10 @@ function Commonspaces() {
     { id: 1, eventsName: "Microwave", icon: <Microwave className="img" /> },
     { id: 2, eventsName: "Dirty Floors", icon: <Dirty className="img" /> },
     { id: 3, eventsName: "Trashcan", icon: <Trash className="img" /> },
-    { id: 4, eventsName: "Door", icon: <Door className="img" /> },
-    { id: 5, eventsName: "General Dirt", icon: <GeneralDirt className="img" /> },
-    { id: 6, eventsName: "Vending Machine", icon: <VendingMachine className="img" /> },
-    { id: 7, eventsName: "Water Fountain", icon: <WaterFountain className="img" /> },
-    { id: 8, eventsName: "Other", icon: <Other className="img" /> },
+    { id: 4, eventsName: "General Dirt", icon: <GeneralDirt className="img" /> },
+    { id: 5, eventsName: "Vending Machine", icon: <VendingMachine className="img" /> },
+    { id: 6, eventsName: "Water Fountain", icon: <WaterFountain className="img" /> },
+    { id: 7, eventsName: "Other", icon: <Other className="img" /> },
   ];
 
   return (
