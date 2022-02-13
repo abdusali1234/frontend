8f5b21e1ed62312215aa51e69a6f4366782467c5
Catarina Alves <catarina._3@hotmail.com>
2022-02-13 07:56:07 +0000

    Toiest comments

--- frontend/src/routes/test/Test.jsx
+++ frontend/src/routes/test/Test.jsx
@@ -1,18 +1,26 @@
+// Import css information
 import "./Test.css";
+
+// Import other things
 import React, {useState} from "react";
+
+// Imported saved icons
 import {ReactComponent as Toiletbowl} from "../../icon/Toiletbowl.svg"
 import {ReactComponent as Toiletpaper} from "../../icon/Toiletpaper.svg"
 import {ReactComponent as Dryer} from "../../icon/Dryer.svg"
 import {ReactComponent as Trashcan} from "../../icon/Trashcan.svg"
 import {ReactComponent as Other} from "../../icon/Other.svg"
 
+// Import Elements needed for the page
 import EventElementBox from '../../components/eventElementBox/EventElementBox';
 import EventHead from '../../components/eventHead/EventHead';
 import PopUpSubmit from "../../components/popUpSubmit/PopUpSubmit";
 
+// Construct webpage
 function Test() {
   const [isClose, setIsClose] = useState(false);
   const [eventStatus, setEventStatus] = useState("");
+  // Add the events
   const events = [
     { id: 1, eventsName: "Toiletbowl", icon: <Toiletbowl className="img" /> },
     { id: 2, eventsName: "Toiletpaper", icon: <Toiletpaper className="img" /> },
